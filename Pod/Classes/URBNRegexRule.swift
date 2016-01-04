0752ff28ea16362d54539ee800758a366760edc7
Joe <jridenour@mercury.io>
2016-01-04 14:53:34 -0600

    Cleanup
    - Updated playground to account for generics
    - Updated regex validateValue to fix casting issue

--- Pod/Classes/URBNRegexRule.swift
+++ Pod/Classes/URBNRegexRule.swift
@@ -18,12 +18,12 @@ public class URBNRegexRule: URBNBaseRule, URBNRequirement {
         super.init(localizationKey: localizationKey)
     }
     
-    public override func validateValue<T: AnyObject>(value: T?) -> Bool {
+    public override func validateValue<T>(value: T?) -> Bool {
         if !isRequired && value == nil { return true }
         
         let pred = NSPredicate(format: "SELF MATCHES[cd] %@", self.pattern)
         
-        return pred.evaluateWithObject(value)
+        return pred.evaluateWithObject(value as? AnyObject)
     }
     
     public static let emailPattern = "^(?:(?:(?:(?:[a-zA-Z0-9_!#\\$\\%&'*+/=?\\^`{}~|\\-]+)(?:\\.(?:[a-zA-Z0-9_!#\\$\\%&'*+/=?\\^`{}~|\\-]+))*)|(?:\"(?:\\\\[^\\r\\n]|[^\\\\\"])*\")))\\@(?:(?:(?:(?:[a-zA-Z0-9_!#\\$\\%&'*+/=?\\^`{}~|\\-]+)(?:\\.(?:[a-zA-Z0-9_!#\\$%&'*+/=?\\^`{}~|\\-]+))*)|(?:\\[(?:\\\\\\S|[\\x21-\\x5a\\x5e-\\x7e])*\\])))$"
